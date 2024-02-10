8a1f9518ce579ca5480fd2132547fa5f40ad52fb
funaaya <150566081+funaaya@users.noreply.github.com>
2024-02-10 21:13:29 +0900

    first commit

---
+++ bin/render-build.sh
@@ -0,0 +1,9 @@
+bin/render-build.sh内
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
