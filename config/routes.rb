8b64c8d9b947d16ac00db7e3025aad2c62f558fa
funaaya <150566081+funaaya@users.noreply.github.com>
2024-02-23 14:32:15 +0900

    commit5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  
+  # devise_for :users
   root to: "items#index"
-  resources :items, only:[:index]
+  resources :items do
+    resources :orders, only: [:index, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+ end
 end
