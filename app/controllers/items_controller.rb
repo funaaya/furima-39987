f4bf2cb835c4d066301f1ca728a98e07543124af
funaaya <150566081+funaaya@users.noreply.github.com>
2024-03-20 19:38:37 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: :index
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
    @items = Item.includes(:user).order('created_at DESC')
   end
@@ -36,6 +36,17 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+  
+
+
   def set_item
     @item = Item.find(params[:id])
   end
@@ -44,4 +55,4 @@ class ItemsController < ApplicationController
     def item_params
       params.require(:item).permit(:name, :description, :category_id, :prefecture_id, :price, :item_status_id, :shipping_cost_id, :shipping_date_id ,:image).merge(user_id: current_user.id)
     end
-end
+ end
