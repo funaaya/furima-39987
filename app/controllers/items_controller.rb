36043efc84c50f9751e2c9c58027c6ec5e96e38e
funaaya <150566081+funaaya@users.noreply.github.com>
2024-03-20 19:48:49 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,10 +39,8 @@ class ItemsController < ApplicationController
   def destroy
     if @item.user_id == current_user.id
       @item.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
     end
+    redirect_to root_path
   end
   
 
