18c5372f4702408886ae8e1d515e50cc6402b3f0
funaaya <150566081+funaaya@users.noreply.github.com>
2024-04-02 19:56:09 +0900

    commit

--- spec/factories/order_payments.rb
+++ spec/factories/order_payments.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :order_payment do
-    association :user
     
+
     postcode { Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
     prefecture_id { Faker::Number.between(from: 1, to: 47) }
     city { Faker::Address.city }
